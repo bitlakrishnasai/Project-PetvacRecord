7f358bee773eac6e25decc294856add463f4d1ab
bitlakrishnasai <krishnasai.bitla@gmail.com>
2021-10-19 19:23:46 +0530

    views

--- PetVacRecord/vacRecords/views.py
+++ PetVacRecord/vacRecords/views.py
@@ -4,4 +4,28 @@ from django.shortcuts import render,HttpResponse
 
 def index(request):
     return HttpResponse("<h3>its working</h>")
+
+def login():
+    pass
+
+def addVacRecord():
+    pass
+
+def editRecord():
+    pass
+
+def delVacRecord():
+    pass
+
+def addPet():
+    pass
+
+def removePet():
+    pass
+
+def reminder():
+    pass
+
+
+
     
